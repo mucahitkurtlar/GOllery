73ad3842abdcc9d5fc0b5c6d7c28b52482ede88c
Mucahit Kurtlar <mucahit@tutanota.com>
2020-07-19 18:06:29 +0300

    Add simple server code

--- main.go
+++ main.go
@@ -1,1 +1,37 @@
 package main
+
+import (
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/gorilla/mux"
+)
+
+var templates *template.Template
+
+func main() {
+	templates = template.Must(template.ParseGlob("templates/*.html"))
+	fs := http.FileServer(http.Dir("./static/"))
+	r := mux.NewRouter()
+	r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", fs))
+
+	r.HandleFunc("/", indexHandler)
+	http.Handle("/", r)
+
+	fmt.Println("Listening on port 8080")
+	srv := &http.Server{
+		Handler:      r,
+		Addr:         ":8080",
+		WriteTimeout: 15 * time.Second,
+		ReadTimeout:  15 * time.Second,
+	}
+	log.Fatal(srv.ListenAndServe())
+}
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	templates.ExecuteTemplate(w, "index.html", nil)
+}
